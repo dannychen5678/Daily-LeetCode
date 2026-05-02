a4ce2329cc85e216d11fa73607282672045b70b1
陳主星 <166821393+dannychen5678@users.noreply.github.com>
2026-05-02 17:30:18 +0800

    Time: 0 ms (100%), Space: 9 MB (38.07%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,37 +1,39 @@
-#include <unordered_map>
-#include <vector>
-#include <string>
-
-using namespace std;
-
-class Solution {
+#include<vector> //動態陣列容器
+#include<unordered_map>//使用hash table
+class Solution
+//當右括號進來時必須與最後的左括號配對,如果相同則return true,不同則return false
+//當右括號進來時,如果沒有剩餘左括號則return false
+{
 public:
-    bool isValid(string s) {
-        // 1. 建立對應表（記得分號）
-        unordered_map<char, char> bracket = {
-            {'(', ')'},
-            {'[', ']'},
-            {'{', '}'}
+    bool isValid(string s) 
+    {
+        vector<char> temp;//用來暫時儲存左括號
+        unordered_map<char,char>bucket
+        {
+            {'(',')'},
+            {'[',']'},
+            {'{','}'},
         };
+        for(int i=0;i<s.length();i++)
+        {
         
-        vector<char> temp;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
-                temp.push_back(s[i]);
-            } 
-            else {
-                if (temp.empty()) return false;
-                
-                // 比對最後一個進入的左括號所對應的右括號
-                if (s[i] != bracket[temp[temp.size() - 1]]) {
-                    return false;
-                } 
-                temp.pop_back(); 
-            }
+           if(s[i]=='('||s[i]=='['||s[i]=='{') //左括號處理
+           {
+             temp.push_back(s[i]);//丟入臨時陣列
+           }
+           else
+           {
+              if(temp.size()>0&&bucket[temp[temp.size()-1]]==s[i])
+              //檢查temp為空發生的時間點是右括號要配對時,所以是在else裡面檢查
+              {
+                temp.pop_back(); //移除最後一個左括號
+              }
+              else
+              {
+                return false;
+              }
+           }
         }
-
-        // 這裡是最精簡的寫法：如果空的就回傳 true，否則 false
-        return temp.empty(); 
-    } // <--- 這是結束 isValid 函式的括號
-}; // <--- 這是結束 class Solution 的括號與分號
+        return temp.empty();
+    }
+};
