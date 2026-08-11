6cf05c2a0dc8d6eda97a94c94b2ad75d7134d994
陳主星 <166821393+dannychen5678@users.noreply.github.com>
2026-08-12 07:19:29 +0800

    Time: 7 ms (13.19%), Space: 10.7 MB (17.84%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,35 +1,38 @@
-class Solution 
-{
+class Solution {
 public:
-    bool isPalindrome(int x)
+    bool isPalindrome(int x) 
     {
-        if(x<0) return false; //處理負數 
-        if(x==0) return true;//0本身也是回文
-        int count=0;
-        int temp=x;    //宣告temp暫存x,為了不破壞原本x    
-        while(temp!=0)
+     
+     if(x<0)return false;
+     if(x==0)return true;   //不斷/10的方法會排除掉0所以要額外設立條件
+     int count=0;
+     int temp;
+     temp=x; //x賦值給temp
+
+     while(temp!=0)//計算位數(不知道迴圈次數所以用while)
+     {
+        temp/=10;
+        count++;
+     }
+
+     vector<int>nums(count);//知道幾位之後,代表我們知道大小了所以宣告一個動態陣列(大小由剛剛得出的count所決定)
+
+     temp=x;//將temp內容從新初始化並且一樣從新賦值x
+     
+     for(int i=count-1;i>=0;i--)//將temp的數字由後往前賦給容器nums,使用for迴圈
+     {
+        nums[i]=temp%10;//取最後一位(%10的餘數除法)
+        temp/=10;//捨棄最後一位
+     }
+     for(int j=0;j<count;j++)//使用nums本身的索引不同完成 ex.長度3但實際到索引2
+     {
+        if(nums[j]!=nums[count-1-j])//與nums自身作比對即可
         {
-         temp/=10;//計算位數
-         count++;
-        }
-        vector<int>nums(count);//宣告動態陣列用於比對
-        temp=x;//重新初始化temp
-        for(int i=count-1;i>=0;i--)//將temp內容複製到nums
-        {
-            nums[i]=temp%10;
-            temp/=10;
-        }
-        for(int i=0;i<count/2;i++) //進行比對(使用自身nums陣列即可)
-        {
-          if(nums[i]!=nums[count-1-i])//nums最後一個count-1
-          {
             return false;
-          }
-        }
-        return true;
+        } 
+     
+     }
+     return true;
 
     }
 };
-//確定array長度
-//取得總長度一半的元素到新array
-//逐項比對
