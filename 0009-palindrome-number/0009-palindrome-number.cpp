d306c72ca276f8523ec6d64d43e7b43d645bf548
陳主星 <166821393+dannychen5678@users.noreply.github.com>
2026-08-22 22:59:13 +0800

    Time: 0 ms (100%), Space: 8.7 MB (19.65%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,42 +1,35 @@
-class Solution {
+//1.排除掉負數以及末位為0的數
+//2.建立反轉字串
+//3.藉由x跟r的關係判斷是否是回文
+class Solution
+{
 public:
-    bool isPalindrome(int x) {
-        // 1.負數必不為迴文
-        // 2.末位為0的除了0以外都不是回文
-        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+    bool isPalindrome(int x)
+{
+if(x<0||(x%10==0&&x!=0))
+{
+    return false;
+}
 
-        int revertedNumber = 0;
-        //不管奇數或偶數只需要處理到中間位數即可(x>r)
-        while (x > revertedNumber) {
-            revertedNumber = revertedNumber * 10 + x % 10;
-            //*10是為了將現有數字左移一位空出個位數字
+int r=0;
 
-            /*
-            (奇數處理)
-            x=121 r=0 進入loop       
-            x=12 r=1 繼續
-            x=1 r=12  結束
+//迴圈直到r>x或r==x的時候(x<=r),r>x是位數為奇數時,r==x是位數為偶數時
+while(x>r)
+{
+    r=x%10+r*10; //把x最後一位移到r最後一位
+    x/=10;
+}
 
-            x=r/10
-
-            */
-            x /= 10;
-
-            /*
-            x=1221 r=0 進入loop
-            x=122  r=1 繼續
-            x=12   r=12 結束
-
-            x=r
-            */
-        }
-
-        //藉由最終x==r或者x==r/10d判斷是否回文
-
-        // 偶數位數時：兩者應完全相同（例如 1221 -> x=12, revertedNumber=12）
-        // 奇數位數時：去掉中間位數後應相同（例如 12321 -> x=12, revertedNumber=123）
-        if( x == revertedNumber || x == revertedNumber / 10)
-        return true;
-        else return false;
-    }
+//判斷回文,並且根據r偶數,奇數處理有所不同
+//r/10是奇數位數的中間位數本來就是對的所以直接刪掉
+if(x==r||x==r/10)
+{
+    return true;
+}
+else
+{
+    return false;
+}
+}
+      
 };
