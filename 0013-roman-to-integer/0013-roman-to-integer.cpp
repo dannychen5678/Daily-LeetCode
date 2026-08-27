2eb42d3375526c70637b4fa7496c43d245258ac7
陳主星 <166821393+dannychen5678@users.noreply.github.com>
2026-08-27 18:10:21 +0800

    Time: 2 ms (78.31%), Space: 10.5 MB (75.03%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,19 +1,9 @@
-//觀念核心:這題只會發生兩件事,插入得比自己小->增加 插入的比自己大->減少(且只有I,X,C可以,VLDM只能插小的),因為VLDM遵守只能插小的條件所以只要針對IXC插大做處理即可
-//ex. I   可以:插比自己小,插比自己大
-//ex. V   可以:插比自己小 不會發生:插比自己大(不用管他)
-//ex.結論=>兩者皆可:插比自己小 I可以插大,V,所以會有一個判斷式
-class Solution
+class Solution 
 {
 public:
-    int romanToInt(string s) 
+    int romanToInt(string s)
     {
-        int sum=0;
-        int currentindex;
-        int nextcurrentindex;
-        //透過位置來比對所以使用vector而非unordered_map
-        //vector透過位置來索引值,unordered_map則是透過key
-        vector<pair<char,int>>list=
-        //pair=>把兩個不同類型的資料綁在一起成為一組完整的資料
+        vector<pair<char,int>> list
         {
             {'I',1},
             {'V',5},
@@ -21,43 +11,42 @@ public:
             {'L',50},
             {'C',100},
             {'D',500},
-            {'M',1000}
+            {'M',1000},
         };
-        //我們要透過s[i]在list中的位置來完成數值總和
-            //我們不知道s[i]在list裡面的位置,所以要先將s[i]與list每一組資料的key做比對
-        for(int i=0;i<s.size();i++) //最外層迭代s[i],內層迴圈迭代list
+        int currentindex;
+        int sum=0;
+        int nextindex;
+
+        //1.找出s[i]在list中的位置,並記錄他的位置=>外層迴圈迭代s[i],內層迴圈迭代list[j]
+        for(int i=0;i<s.length();i++)
         {
-            //1.先找s[i]實際在list中的位置,是第幾個?
-            for(int j=0;j<list.size();j++)
+            for(int j=0;list.size();j++)
             {
-                if(s[i]==list[j].first)//透過與逐個list[j]的key比對(使用first屬性取得list[j] 的key
+                if(s[i]==list[j].first)
                 {
-                   currentindex=j; //如果相同就記錄該位置到一個新的變數,這個變數會當作list的索引,不同繼續找
-                   break;
+                    currentindex=j;
+                    break;
                 }
             }
-            //2.剛剛找出s[i]list的位置後,先做判別s[i]是否是s最後一個,是的話直接加入總和到sum,
-            //並且直接return sum;
-
-            if(i==s.size()-1)
+            //2.判斷s[i]是否是s的最後一個
+            if(i==s.length()-1)
             {
                 sum+=list[currentindex].second;
                 return sum;
             }
-            //3.尚未清楚s[i+1]在list中的位置先取得後才能作判別
+
+            //3.取得s[i+1]在list中的位置
             for(int k=0;k<list.size();k++)
             {
                 if(s[i+1]==list[k].first)
                 {
-                    nextcurrentindex=k;
+                    nextindex=k;
                     break;
                 }
-
             }
-
-
-            //4.判別s[i+1]在list中的位置是否在s[i]之上,是的話sum-list[currentindex],else 加入sum
-            if(currentindex<nextcurrentindex)
+            
+            //4.判斷s[i+1]在list中的位置是否>s[i]在list中的位置
+            if(nextindex>currentindex)
             {
                 sum-=list[currentindex].second;
             }
@@ -65,10 +54,7 @@ public:
             {
                 sum+=list[currentindex].second;
             }
-            
         }
         return sum;
-        
-        
     }
 };
