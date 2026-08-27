b6af7338b5afb9695873bccc22dc9eb6d50887bb
陳主星 <166821393+dannychen5678@users.noreply.github.com>
2026-08-27 18:25:52 +0800

    Time: 4 ms (66.82%), Space: 13.6 MB (6.05%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,60 +1,43 @@
-class Solution 
+//透過值的大小來解
+
+class Solution
 {
 public:
     int romanToInt(string s)
     {
-        vector<pair<char,int>> list
+      //使用hash表
+      unordered_map<char,int> list
+      {
+        {'I',1},
+        {'V',5},
+        {'X',10},
+        {'L',50},
+        {'C',100},
+        {'D',500},
+        {'M',1000},
+      };
+      int sum=0;
+      //1.判斷s[i]是否是s中最後一個
+      for(int i=0;i<s.length();i++)
+      {
+        if(i==s.length()-1)
         {
-            {'I',1},
-            {'V',5},
-            {'X',10},
-            {'L',50},
-            {'C',100},
-            {'D',500},
-            {'M',1000},
-        };
-        int currentindex;
-        int sum=0;
-        int nextindex;
-
-        //1.找出s[i]在list中的位置,並記錄他的位置=>外層迴圈迭代s[i],內層迴圈迭代list[j]
-        for(int i=0;i<s.length();i++)
-        {
-            for(int j=0;list.size();j++)
-            {
-                if(s[i]==list[j].first)
-                {
-                    currentindex=j;
-                    break;
-                }
-            }
-            //2.判斷s[i]是否是s的最後一個
-            if(i==s.length()-1)
-            {
-                sum+=list[currentindex].second;
-                return sum;
-            }
-
-            //3.取得s[i+1]在list中的位置
-            for(int k=0;k<list.size();k++)
-            {
-                if(s[i+1]==list[k].first)
-                {
-                    nextindex=k;
-                    break;
-                }
-            }
-            
-            //4.判斷s[i+1]在list中的位置是否>s[i]在list中的位置
-            if(nextindex>currentindex)
-            {
-                sum-=list[currentindex].second;
-            }
-            else
-            {
-                sum+=list[currentindex].second;
-            }
+            sum+=list[s[i]];
+            break;
         }
-        return sum;
+        
+        //2.判斷s[i+1]的值是否>s[i]
+
+        else if(list[s[i+1]]>list[s[i]])
+        {
+            sum-=list[s[i]];
+        }
+        else
+        {
+            sum+=list[s[i]];
+        }
+        
+      }
+      return sum;
     }
 };
