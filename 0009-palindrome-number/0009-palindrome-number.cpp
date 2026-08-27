7caf688df07eb370f09ab7d3e34d69771466a5c3
陳主星 <166821393+dannychen5678@users.noreply.github.com>
2026-08-27 18:53:18 +0800

    Time: 0 ms (100%), Space: 8.5 MB (92.14%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,36 +1,52 @@
-//1.排除掉負數以及末位為0的數
-//2.建立反轉字串
-//3.藉由x跟r的關係判斷是否是回文
 class Solution
 {
 public:
-    bool isPalindrome(int x)
-{
-if(x<0||(x%10==0&&x!=0))
-{
-    return false;
-}
+    bool isPalindrome(int x) 
+    {
+       
+        //1.排除掉負數,以及末位為0(0本身為回文)的數字
+        if(x<0||(x%10==0&&x!=0))
+        {
+            return false;
+        }
+        //2.建立反轉字串=>r*10向右增加一位,個位數字空出來 ,以及x%10取得x最後一位
+        int r=0;
+        while(x>r)
+        {
+            r=r*10+x%10;
+            x/=10;
+        }
+        //奇數位的結果會是x<r
+        //偶數位的結果會是x==r
 
-int r=0;
+        //3.與原始數字做比對,奇跟偶處理方式不同
 
-//迴圈直到r>x或r==x的時候(x<=r),r>x是位數為奇數時,r==x是位數為偶數時
-//奇數位的結束條件是當x<r時,而偶數位的結束條件是當x==r時
-while(x>r)
-{
-    r=x%10+r*10; //把x最後一位移到r最後一位
-    x/=10;
-}
+        //第一層做奇數位還是偶數位判斷,第二層做真實數字的比對
+        //奇數位
+        if(x<r) 
+        {
+           if(x==r/10) //r/10與x做比對
+           {
+            return true;
+           }
+           else
+           {
+            return false;
+           }
+        }
+        //偶數位
+        else
+        {
+            if(x==r)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
-//判斷回文,並且根據r偶數,奇數處理有所不同
-//r/10是奇數位數的中間位數本來就是對的所以直接刪掉
-if(x==r||x==r/10)
-{
-    return true;
-}
-else
-{
-    return false;
-}
-}
-      
+
+    }
 };
