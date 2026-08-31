74aa36cb6f5370b30b6bf65a98e22da24128b020
陳主星 <166821393+dannychen5678@users.noreply.github.com>
2026-08-31 16:30:26 +0800

    Time: 0 ms (100%), Space: 11.7 MB (80.12%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -47,11 +47,12 @@ public:
     }
 };
 
-//迴圈裡面要先處理prefix先跑完的情況=>ex.prefix:flow strs[i]:flower
-//接著處裡正常情況prefix比strs[i]還長,或是相等長度
-//而正常情況又分為:strs[i]與prefix都比對無誤,以及中途確實有不同
+//三種情況
 /*
 ex.
+
+超出邊界情況=>
+prefix比strs[i]早結束=>prefix維持原先不動,還是strs[0]。,因為這樣代表prefix的字元都與strs[i]相同而題目球的是最常共同前綴子串,所以維持原先即可
 
 中途有不同=>
 prefix:flower
@@ -64,5 +65,40 @@ strs:flow
 處理順序是中途有不同以然後才是都相等
 
 中途有不同就是索引到不相同的字元的前一個位置然後當作prefix
-都相同就是prefix不變
+都相同的就是prefix要改成strs[i]=>更新新的prefix成strs[i]
+
+處理順序優先級=>
+1.超出邊界情形
+2.中途不同
+3.都相等
 */
+
+/*
+步驟
+1.取得字串vector大小透過.size()=>
+判斷是甚麼資料型態=>array? vector?
+
+array=>透過sizeof(strs)/sizeof(strs[i])
+
+vector=>透過.size()方法
+
+2.vector第一個當成prefix
+=>std::string prefix=strs[0];
+
+3.透過flag來監控事件當下的狀態,初始狀態是false
+
+3.迭代vector(容器)中的元素,直到strs.length()-1結束
+
+4.迭代每一個元素(字串)中的每一個字元
+
+**5.處理超出邊界情形(i>prefix本身)=>prefix不變
+
+**6.處理不同字元情形=>j位置之前的字串設為新的prefix
+
+**7.比對都相同(strs[i]與prefix)=>strs[i]當作新的prefix
+
+8.都結束後最後跳出迴圈return新的prefix
+
+*/
+
+
